fb82be325093dfb6b41f85229b3cdbc1479f8e42
David F <figueroasalasdavidchoco@gmail.com>
2025-09-16 15:33:33 -0500

    form is activate

--- src/signup.php
+++ src/signup.php
@@ -10,6 +10,21 @@ $e_mail = $_POST['email'];
 $p_wd = $_POST['passwd'];
 
 $ecn_pass = password_hash($p_wd, PASSWORD_DEFAULT );
+$check_email=" 
+SELECT 
+    u.email
+FROM
+    users u
+WHERE
+    email = '$e_mail' or ide_number = '$id_number'
+LIMIT 1    
+";
+$res_check = pg_query($conn, $check_email);
+if(pg_num_rows($res_check) > 0){
+    echo"<script>alert('User already exits !!')</script>";+
+    header('refresh:0;url=signup.html');
+
+} else{
 //step 3. create query to insert into
 $query="
 insert into users(
@@ -27,8 +42,12 @@ $res = pg_query($conn, $query);
 //step 5. Validate result
 
 if($res){
-    echo "User has been created successfully !!!";
+    //echo "User has been created successfully !!!";
+    echo"<script>alert('Success !!! Go to login')</script>";+
+    header('refresh:0;url=signin.html');
 }else{
     echo "something wrong!";
 }
+}
+
 ?>
