ef2fc24447f07be94b09f65470a120ada23b63b7
David F <figueroasalasdavidchoco@gmail.com>
2025-10-14 15:36:11 -0500

    list users UI

---
+++ src/list_users.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Marketapp - list_users</title>
+</head>
+<body>
+    <table border="1" align="Center";>
+     <tr>
+        <th>Full name</th>
+        <th>E-mail</th>
+        <th>Ide. number</th>
+        <th>Phone number</th>
+        <th>Status</th>
+        <th>Options</th>
+        
+</tr>
+<tr>
+        <td>Joe Doe</td>
+        <td>JoeDoe@gmail.com</td>
+        <td>165413130</td>
+        <td>3165651</td>
+        <td>Active</td>
+        <td>
+            <a href="#"><img src="icons/lupa.png" width="20"></a>
+            <a href="#"><img src="icons/lapiz.png" width="20"></a>
+            <a href="#"><img src="icons/papelera.png" width="20"></a>
+        </td>
+        
+</tr>
+    </table>    
+</body>
+</html>
